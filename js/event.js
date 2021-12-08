b2eb9918c7ad1ca10cfb013f78fd322617fa3486
Rachel Lee <rachel318318@gmail.com>
2021-12-09 00:04:45 +0900

    back to original

--- js/event.js
+++
@@ -1,7 +0,0 @@
-captureVideo = () =>{
-  captureSteelShot();
-  renderImg();
-}
-
-document.querySelector("video").addEventListener('click', captureVideo, false);
-document.querySelector("button").addEventListener('click', captureVideo, false);
